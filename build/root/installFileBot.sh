129a2395e6567e9734d3e7085342fb3c0a06721b
1852285+alexmerm@users.noreply.github.com <1852285+alexmerm@users.noreply.github.com>
2020-12-24 02:35:16 -0500

    Fixed build script

--- build/root/installFileBot.sh
+++ build/root/installFileBot.sh
@@ -8,5 +8,4 @@ sh -xu <<< "$(curl -fsSL https://raw.githubusercontent.com/filebot/plugins/maste
 #Symlink it again b/c the script uses sudo which arch doesnt have
 ln -sf "$PWD/filebot.sh" /usr/local/bin/filebot
 #Download the Pushover Bash
-curl https://raw.githubusercontent.com/akusei/pushover-bash/master/pushover.sh -o /home/nobody/pushover.sh
-chmod +x /home/nobody/pushover.sh
+curl https://raw.githubusercontent.com/akusei/pushover-bash/master/pushover.sh -o /home/nobody/pushover.sh && chmod +x /home/nobody/pushover.sh
